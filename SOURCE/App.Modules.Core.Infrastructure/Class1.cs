78aaf7c92701f7147c6908f36a04e9ee24223529
Sky S <SigalS@moe.govt.nz>
2022-10-17 15:53:29 +1300

    Created solution

---
+++ SOURCE/App.Modules.Core.Infrastructure/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Modules.Core.Infrastructure
+{
+    /// <summary>
+    /// Temp Doc
+    /// </summary>
+    public class Class1
+    {
+
+    }
+}
---
+++ SOURCE/App.Modules.Core.Presentation.Web/Api/OData/Controllers/Implementations/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Modules.Core.Presentation.Web.Api.OData.Controllers.Implementations
+{
+    /// <summary>
+    /// Temp Doc
+    /// </summary>
+    public class Class1
+    {
+
+    }
+}
